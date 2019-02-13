0affb391d2f8e820808c412cba54b3841467029f
Josh Beard <josh@jlbeard.com>
2019-02-12 22:17:16 -0500

    Fixed stats issue

--- src/message_handlers/request_character_data_handler.js
+++ src/message_handlers/request_character_data_handler.js
@@ -2,28 +2,30 @@ import { calculateStats } from "../character_data";
 
 export function request_character_data_handler(gameObject, client, serverData) {
 
-    // this will be pulled from a local database
     const tempPlayer = {
-        type: "player_data",
-        clientId: client.clientId,
-        value: {
-            name: 'Filius',
-            sheet: 'assets/filius_sheet.png',
-            level: 1,
-            currentclass: 'jester',
-            clvl: {
-                jester: 1,
-                warrior: 1,
-                priest: 1,
-                thief: 1,
-                mage: 1,
-                budoka: 1,
-            },
-            equipment: {}
-        }
+        name: 'Filius',
+        sheet: 'assets/filius_sheet.png',
+        level: 1,
+        currentClass: 'jester',
+        clvl: {
+            jester: 1,
+            warrior: 1,
+            priest: 1,
+            thief: 1,
+            mage: 1,
+            budoka: 1,
+        },
+        equipment: {}
     };
 
     calculateStats(tempPlayer);
 
-    return tempPlayer;
+    // this will be pulled from a local database
+    const playerResponse = {
+        type: "player_data",
+        clientId: client.clientId,
+        value: tempPlayer
+    };
+
+    return playerResponse;
 }

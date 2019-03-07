0da46c54b26f52ebcba7a9d690aa2f51d8d4932b
bferguson3 <32964659+bferguson3@users.noreply.github.com>
2019-03-07 09:43:06 -0500

    Update update-lobby-char.handler.ts
    forgot 'break' inside lobby search

--- src/message-handlers/update-lobby-char.handler.ts
+++ src/message-handlers/update-lobby-char.handler.ts
@@ -29,6 +29,7 @@ export class UpdateLobbyCharacterHandler extends MesssageHandlerBase {
                         break;
                     }
                 }
+                break;
             }
         }
 

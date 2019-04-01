59f2f295851168ad1b330de308c2f76f4561dc1c
Josh Beard <josh@jlbeard.com>
2019-04-01 16:29:33 -0400

    Added api method to delete players #60

--- dqc-web/server/routes/player.js
+++ dqc-web/server/routes/player.js
@@ -12,7 +12,7 @@ router.post('/new', (req, res) => {
 
         const name = req.body.name;
         const gender = req.body.gender;
-        const currentClass = req.body.class;
+        const currentClass = req.body.currentClass;
 
         const clvl = {
             jester: 0,
@@ -22,6 +22,11 @@ router.post('/new', (req, res) => {
             mage: 0,
             budoka: 0
         };
+
+        if (!name || !gender || !currentClass) {
+            res.status(500).send("One or more required fields are missing");
+            return;
+        }
 
         // get user
         if (!fs.existsSync(`${path}/${username}.json`)) {
@@ -45,6 +50,56 @@ router.post('/new', (req, res) => {
             equipment: {},
             clvl: clvl
         });
+
+        const fileData = JSON.stringify(parsedData);
+
+        fs.writeFileSync(`${path}/${username}.json`, fileData, 'utf8');
+
+        res.status(200).send();
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
+router.delete('', (req, res) => {
+    try {
+        const username = req.body.username;
+        const playerName = req.body.playerName;
+        const playerIndex = req.body.playerIndex;
+
+        if (!username || !playerName || !playerIndex) {
+            res.status(500).send("Missing fields needed to find character to delete");
+            return;
+        }
+
+        // get user
+        if (!fs.existsSync(`${path}/${username}.json`)) {
+            res.status(500).send("User does not exist");
+            return;
+        }
+
+        const data = fs.readFileSync(`${path}/${username}.json`, "utf8");
+        const parsedData = JSON.parse(data);
+
+        if (!parsedData.playerData) {
+            res.status(500).send("No character data to delete");
+            return;
+        }
+
+        if (!parsedData.playerData.length <= playerIndex) {
+            res.status(500).send("Invalid index");
+            return;
+        } 
+
+        const indexedPlayer = parsedData.playerData[playerIndex];
+
+        if (indexedPlayer.name === playerName) {
+            parsedData.playerData.splice(playerIndex, 1);
+        } else {
+            res.status(500).send("Invalid character name");
+            return;
+        }
 
         const fileData = JSON.stringify(parsedData);
 

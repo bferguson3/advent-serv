b4f096a6e3179314977440ed32d91f3e5b832558
Josh Beard <josh@jlbeard.com>
2019-04-08 15:20:45 -0400

    Added ephemeral stats to player data object #67

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -3,4 +3,5 @@ export { ErrorType } from "./error-type.enum";
 export { GenderType } from "./gender-type.enum";
 export { RequestMessageType } from "./request-message-type.enum";
 export { ResponseMessageType } from "./response-message-type.enum";
+export { StatusEffectType } from "./status-effect-type.enum";
 export { VisibilityLevelType } from "./visibility-level-type.enum";

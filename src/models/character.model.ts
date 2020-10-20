0273bac025497a18f5528fdafa8d2f16ccc7d5ea
bferguson3 <32964659+bferguson3@users.noreply.github.com>
2020-10-20 18:24:52 -0400

    Update character.model.ts

--- src/models/character.model.ts
+++ src/models/character.model.ts
@@ -1,5 +1,7 @@
+import { Vector3 } from "./vector-3.model";
 import { Vector4 } from "./vector-4.model";
 
 export abstract class Character {
-    public pos: Vector4;
+    public pos: Vector3;
+    public rot: Vector4;
 }

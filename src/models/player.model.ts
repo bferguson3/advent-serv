155766a53b28cbaeb01e3cba0310be2e6faa52dc
bferguson3 <32964659+bferguson3@users.noreply.github.com>
2020-10-20 18:22:16 -0400

    Update player.model.ts
    change pos/rot types

--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -1,12 +1,15 @@
 import { Character } from "./character.model";
+import { Vector3 } from "./vector-3.model";
 import { Vector4 } from "./vector-4.model";
 
 export class Player extends Character {
     public id: number;
     public lastActivity?: number;
-    public lHandPos: Vector4;
+    public lHandPos: Vector3;
+    public lHandRot: Vector4;
     public lHandObj: string;
-    public rHandPos: Vector4;
+    public rHandPos: Vector3;
+    public rHandRot: Vector4;
     public rHandObj: string;
     public faceTx: string;
     public bodyTx: string;

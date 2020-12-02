842bc010677c13b763f5adb01970b968ecd835bb
Josh Beard <josh@jlbeard.com>
2020-12-01 23:38:31 -0500

    Removed ping handler

--- src/message-handlers/index.ts
+++ src/message-handlers/index.ts
@@ -1,4 +1,3 @@
-export { LoginMessageHandler } from "./login-message-handler.handler"; 
+export { LoginMessageHandler } from "./login-message-handler.handler";
 export { MessageHandlerBase } from "./message-handler-base.handler";
-export { PingMessageHandler } from "./ping-message-handler.handler";
 export { UpdateMessageHandler } from "./update-message-handler.handler";
